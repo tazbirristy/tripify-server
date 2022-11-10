f4ea16a2482f90a670f988dcc45c6b73f1421e95
tazbirristy <tazbirristyweb@gmail.com>
2022-11-10 15:38:55 +0600

    server connected to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,14 @@ const app = express();
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2s00ahv.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Tripify server is running");
